eb6723b3ac2ea52da2854646340c9591e6e7b34d
BenjaminFont <benjamin.font_pera@hotmail.de>
2025-04-06 23:55:00 +0200

    fixed changed for routing issue

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,25 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
+import fs from "fs";
 
 import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    {
+      name: "copy-static-json",
+      closeBundle() {
+        fs.copyFileSync(
+          path.resolve(__dirname, "static.json"),
+          path.resolve(__dirname, "dist/static.json")
+        );
+      },
+    },
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
